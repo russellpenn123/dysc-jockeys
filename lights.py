e69311a4e0ceb6b5a2918f85b89e072566e809fd
Pangolin_devPi <pangolinpaw@hotmail.co.uk>
2023-11-03 14:27:58 +0000

    adjust imports

--- lights.py
+++ lights.py
@@ -1,6 +1,6 @@
 import board
 import neopixel
 
-#                   PIN     LED Count
-pixels = NeoPixel(Board.D18, 60)
+#                 	    PIN     LED Count
+pixels = neopixel.NeoPixel(board.D18, 60)
 pixels.fill((0,255,0))
